bd7ce179a584b7e6090235644c685c936d970dc9
shunichi.horigome@ipmu.jp from Ryzen3990X <shunichi.horigome@ipmu.jp>
2025-12-11 15:17:41 +0900

    Remove legacy files

--- IH_errors.py
+++
@@ -1,9 +0,0 @@
-
-class StatusException(BaseException):
-    """
-    Is raised when the global status of Insert Halo indicates that some process is still running while another process is being called.
-    """
-    def __init__(self, message):
-        self.message = message
-        super().__init__(self.message)
-
