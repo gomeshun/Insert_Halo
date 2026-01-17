bdb566bb049474a23631e50b42eed83a483ae5d5
Stefan-V22 <stefanverhoeven4@gmail.com>
2026-01-17 18:05:04 +0100

    Insert_Halo version 0.30
    Added the ability to easily generate Lenstronomy lensmodels and host halo generation calculations as well as several bug fixes related to the rerun function and the spatial generation code.

---
+++ Insert_Halo_V0.30/IH_errors.py
@@ -0,0 +1,25 @@
+
+class StatusException(BaseException):
+    """
+    Is raised when the global status of Insert Halo indicates that some process is still running while another process is being called.
+    """
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+class StageException(BaseException):
+    """
+    Is raised when the current function of Insert Halo requires some previous computation to have been run first, which has not happened.
+    """
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+class SettingsException(BaseException):
+    """
+    Is raised when some (internal) settings of Insert Halo are contradictory or will otherwise cause internal error.
+    """
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
